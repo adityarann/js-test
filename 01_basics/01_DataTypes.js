30bdab18012b8592374f4884cb8433ccdf09163b
adityarann <125434299+adityarann@users.noreply.github.com>
2023-03-22 16:13:20 +0000

    data types

---
+++ 01_basics/01_DataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(2
+    +3);// code readablitya should be high
+
+     console.log("Aditya")
+
+ let name = "Aditya"
+ let age = 21
+ let isLogedIn = false
+ 
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId =45554
+let accountEmail = "adityagoogle@.com"
+var accountpassword = 12345
+
+// accountId  = 2//not allowed
+accountEmail = "kvs@fvkvmf"
+accountpassword = 23546
+
+console.log(accountId);
+console.table( [accountEmail,accountId,accountpassword]);
